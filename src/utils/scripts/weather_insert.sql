c5744a1565f9625df47710152cada766c26b343e
FawenYo <sky80838@gmail.com>
2023-03-26 09:05:32 +0800

    Change weather data source

--- src/utils/scripts/weather_insert.sql
+++ src/utils/scripts/weather_insert.sql
@@ -1,14 +1,24 @@
 INSERT INTO "weather_history" (
-  station_id, time, temperature, weather_code,
-  wind_speed, wind_degree, pressure,
-  precipitation, humidity, cloudiness,
-  feels_like, visibility, uv_index,
-  wind_gust, is_day, co, no2, o3, so2,
-  pm2_5, pm10, us_epa_index, gb_defra_index
+  station_id, time, temperature, apparent_temperature,
+  relative_humidity, dewpoint, precipitation_probability,
+  precipitation, rain, showers, weather_code,
+  pressure, surface_pressure, cloud_cover,
+  visibility, evapotranspiration,
+  et0_fao_evapotranspiration, vapor_pressure_deficit,
+  wind_speed, wind_gusts, soil_temperature,
+  soil_moisture, uv_index, uv_index_clear_sky,
+  cape, freezinglevel_height, pm2_5,
+  pm10, carbon_monoxide, nitrogen_dioxide,
+  sulphur_dioxide, ozone, aerosol_optical_depth,
+  dust, us_aqi, us_aqi_pm2_5, us_aqi_pm10,
+  us_aqi_no2, us_aqi_co, us_aqi_o3,
+  us_aqi_so2
 )
 VALUES
   (
     $1, $2, $3, $4, $5, $6, $7, $8, $9, $10,
     $11, $12, $13, $14, $15, $16, $17, $18,
-    $19, $20, $21, $22, $23
+    $19, $20, $21, $22, $23, $24, $25, $26,
+    $27, $28, $29, $30, $31, $32, $33, $34,
+    $35, $36, $37, $38, $39, $40, $41
   )
